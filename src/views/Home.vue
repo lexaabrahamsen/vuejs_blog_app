8d0bd16a9ec490b62ad0d4fd00ec18a9c21a0d9a
Lexa <lexaraea@gmail.com>
2019-08-06 02:36:49 -0400

    all controllers working correctly

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>New Post</h1>
+    <div>
+      Title:
+      <input type="text" v-model="newPostTitle" />
+      Body:
+      <input type="text" v-model="newPostBody" />
+      Image:
+      <input type="text" v-model="newPostImage" />
+      <button v-on:click="createPost()">Create Post</button>
+    </div>
+    <h1>All Posts</h1>
+    <div v-for="post in posts">
+      <img v-bind:src="post.url" v-bind:alt="post.title" />
+      <h2>{{ post.title }}</h2>
+      <p>Body: {{ post.body }}</p>
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Welcome to Blogs!",
+      posts: [],
+      newPostImage: "",
+      newPostTitle: "",
+      newPostBody: ""
+    };
+  },
+  created: function() {
+    axios.get("/api/posts").then(response => {
+      this.posts = response.data;
+    });
+  },
+  methods: {
+    createPost: function() {
+      var params = {
+        title: this.newPostTitle,
+        image: this.newPostImage,
+        body: this.newPostBody
+      };
+      axios.post("/api/posts", params).then(response => {
+        this.posts.push(response.data);
+        this.newPostTitle = "";
+        this.newPostId = "";
+        this.newPostBody = "";
+      });
+    }
   }
-}
+};
 </script>
